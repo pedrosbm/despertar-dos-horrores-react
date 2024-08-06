7268f97afa5d0078a2983c98f8c2e0dd97f35795
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-06 09:13:56 -0300

    pequeno progresso em aula

--- src/components/CharacterCreateSteps/Step1.tsx
+++ src/components/CharacterCreateSteps/Step1.tsx
@@ -3,13 +3,14 @@ import { ChangeEvent } from "react"
 import line from "../../assets/ornamented-line.png"
 import "../../styles/CharacterCreationSteps/Step1.scss"
 import { Character } from "../../types"
+import { CiSquarePlus } from "react-icons/ci";
 
 type StepProps = {
     changeCharacter: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void,
     character: Character | undefined
 }
 
-const Step1 = ({changeCharacter, character} : StepProps) => {
+const Step1 = ({ changeCharacter, character }: StepProps) => {
     return (
         <div className="step step1">
             <div className="title">
@@ -24,11 +25,15 @@ const Step1 = ({changeCharacter, character} : StepProps) => {
                     </div>
                     <div className="inputBox">
                         <label htmlFor="">História</label>
-                        <textarea className="history" required  onChange={changeCharacter} name="historia" placeholder="Era uma vez um principe..."></textarea>
+                        <textarea className="history" required onChange={changeCharacter} name="historia" placeholder="Era uma vez um principe..."></textarea>
                     </div>
                 </div>
-                <div className="cropedImg">
-
+                <div className="characterImage">
+                    {/* TODO implementar easy crop */}
+                    <div className="cropperContainer">
+                        {/* <img src="" alt="" className="characterPreview" /> */}
+                        <CiSquarePlus className="plus" />
+                    </div>
                 </div>
             </div>
         </div>
